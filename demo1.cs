412be39e44b4d397aefd7ca3b9be51a06bef3f08
jyothsna-0110 <jyothsnachennuri1313@gmail.com>
2021-10-06 21:46:31 +0530

    first commit

---
+++ demo1.cs
@@ -0,0 +1,9 @@
+using System;
+class Program     {
+        static void Main(string[] args)         {
+            string str1;
+            string str2 = "welcome";
+            str1 = str1 + " "+ str2;
+            Console.WriteLine(str1);            
+        }
+    }
